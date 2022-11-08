6982c20ab6b15fd2a4eb6a7b901576862c35de44
mjs757 <lszjd757@gmail.com>
2022-11-08 12:49:33 -0800

    html&css subtitle change

--- assets/script.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-
-    var today = moment();
-    
-    $("#currentDay").text(today.format("[Current Date:] dddd MMM Do, YYYY"));
-    $("#currentTime").text(today.format("[Current Time:] h:mm a [PST]"));
-});
