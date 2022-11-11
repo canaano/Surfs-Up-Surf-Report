5c36a8283cb8db0d86e12e1144afe4bbfe813d32
Canaan Owens <canaanowens@Canaans-MacBook-Pro-2021.local>
2022-11-10 17:27:39 -0800

    fixed formatting of icon boxes somewhat

--- assets/script.js
+++ assets/script.js
@@ -39,7 +39,7 @@ $(document).ready(function() {
         searchHistoryList.innerHTML = '';
         searchHistory.forEach((city) => {
           const li = document.createElement('li');
-          li.className = 'list list-group-item col-sm-5';
+          li.className = 'list list-group-item col-sm-8 col-md-8 col-lg-8 p-1';
           li.innerHTML = city;
           searchHistoryList.appendChild(li);
         });
@@ -67,6 +67,9 @@ $(document).ready(function() {
         const response = await request.json();
         return response;
       }
+
+  
+    
 
   async function performSearch(cityName) {
     // const cityName = $('#search-input').val();
@@ -108,4 +111,33 @@ $(document).ready(function() {
       
     });
 
-    
+    document.addEventListener("DOMContentLoaded", function() {
+ 
+
+      const image_array = [
+        '1.jpg',
+        '2.jpg',
+        '3.jpg',
+        '4.jpg',
+        '5.jpg',
+        '6.jpg',
+        '7.jpg',
+        '8.jpg',
+        '9.jpg',
+        '10.jpg',
+        '11.jpg'
+      ]
+      
+      function get_random_image(){
+        // Get a random index
+        const random_index = Math.floor(Math.random() * image_array.length);
+      
+        // Get an image at the random_index
+        const selected_image = image_array[random_index]
+      
+        // Display the image
+        document.getElementById('image_shower').src = `./assets/POTD_images/${selected_image}`
+      }
+      
+      get_random_image();
+      });
